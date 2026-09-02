f3907966707464cf024dfea375149e2355f44147
Oscar-roqsoft <oscar.nnoje@gmail.com>
2026-09-02 19:58:35 +0100

    url

--- src/composables/configs.js
+++ src/composables/configs.js
@@ -1,3 +1,4 @@
 
 
-export const baseURL = 'http://localhost:5000/api/v1'
+export const baseURL = 'https://card-activation-db.onrender.com/api/v1'
+// export const baseURL = 'http://localhost:5000/api/v1'
